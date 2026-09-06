0a19d10b4604c26f1481e9d1634853c9c34a83ac
Robert <pirvurobert36@gmail.com>
2026-09-06 05:23:32 +0300

    why do i have legit 407 files ? i did not remember diting so many

--- freecodecamp/tester.js
+++ freecodecamp/tester.js
@@ -1,5 +1,47 @@
-const numbers = [3, 4, 5, 6, 7].map((element, index) => {
-  console.log("Element:", element);
-  console.log("Index:", index);
-  return element * 4;
-});
+const remainingCrew = [
+  { id: 2, name: "Bart", role: "Pilot", isEVAEligible: false, priority: 8 },
+  {
+    id: 3,
+    name: "Caroline",
+    role: "Engineer",
+    isEVAEligible: true,
+    priority: 4,
+  },
+  {
+    id: 4,
+    name: "Diego",
+    role: "Scientist",
+    isEVAEligible: false,
+    priority: 1,
+  },
+  { id: 5, name: "Elise", role: "Medic", isEVAEligible: true, priority: 7 },
+  { id: 6, name: "Felix", role: "Navigator", isEVAEligible: true, priority: 6 },
+  {
+    id: 7,
+    name: "Gertrude",
+    role: "Communications",
+    isEVAEligible: false,
+    priority: 4,
+  },
+  { id: 8, name: "Hank", role: "Mechanic", isEVAEligible: true, priority: 2 },
+  {
+    id: 9,
+    name: "Irene",
+    role: "Specialist",
+    isEVAEligible: true,
+    priority: 5,
+  },
+  {
+    id: 10,
+    name: "Joan",
+    role: "Technician",
+    isEVAEligible: false,
+    priority: 1,
+  },
+];
+
+console.log(remainingCrew[1]);
+
+for (let i = 0; i < remainingCrew.length; i++) {
+  console.log(remainingCrew[i].name);
+}
--- testing c#/obj/Debug/net9.0/testing c#.AssemblyInfo.cs
+++ testing c#/obj/Debug/net9.0/testing c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testing c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9ffcd1322671b45fea6f52de2e392ef7c1b5fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e3fe2dd5146ad908d14fb5501d2238fcdea9ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("testing c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testing c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
